434373db8a6f4afbfd94ec2ea8e6812e7632a4de
Roderick Montague <roderick.montague243@gmail.com>
2020-12-15 18:29:35 -0500

    Added StartScene
    The start scene has been added. A means of going back to the start scene from the main scene has not as of yet.

---
+++ Assets/_Scripts/EX/SceneChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// changes the scene
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // changes the scene using its name
+    public void ChangeScene(string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    // changes the scene using its number.
+    public void ChangeScene(int newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
