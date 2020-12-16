34d23333a7fd9a5becca13748d3577937df72e6c
Roderick Montague <roderick.montague243@gmail.com>
2020-12-16 14:50:02 -0500

    Start of Bullet Factory
    The start of the bullet factory. I'm probably going to just finish up the DLL but not test it, then get back to this.

---
+++ Assets/_Scripts/EX/BulletFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletFactory
+{
+    // instance of bullet pool manager.
+    private static BulletFactory instance = null;
+
+    // constructor
+    private BulletFactory()
+    {
+        Start();
+    }
+
+    // gets instance of class (for bonus)
+    public static BulletFactory GetInstance()
+    {
+        if (instance == null) // no instance generated.
+        {
+            instance = new BulletFactory(); // creates instance
+        }
+
+        return instance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
